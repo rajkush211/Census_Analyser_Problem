d2d03cfa6973694bb029d837f7f8aa16efda8e6b
rajkush211 <rajkush211.rk@gmail.com>
2020-04-07 05:57:31 +0530

    ADDED : Code should throw Exception when provided CSV file contains wrong delimiter

--- src/test/java/censusanalyser/StateCodeAnalyserTest.java
+++ src/test/java/censusanalyser/StateCodeAnalyserTest.java
@@ -8,6 +8,8 @@ public class StateCodeAnalyserTest {
     private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
     private static final String WRONG_CSV_FILE_TYPE = "./src/test/resources/IndianStateCodeTypeWrong.txt";
+    private static final String WRONG_CSV_FILE_DELIMITER = "./src/test/resources/IndiaStateCodeDelimiterWrong.csv";
+
     @Test
     public void givenIndiaStateCodeCSVFile_ShouldReturnsCorrectRecords() {
         try {
@@ -34,6 +36,16 @@ public class StateCodeAnalyserTest {
             censusAnalyser.loadIndiaStateData(WRONG_CSV_FILE_TYPE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_FILE_TYPE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaStateCodeCSV_WhenWrongDelimiter_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaStateData(WRONG_CSV_FILE_DELIMITER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_FILE_DELIMITER, e.type);
         }
     }
 }
